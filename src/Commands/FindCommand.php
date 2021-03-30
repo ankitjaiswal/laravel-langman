262662f7758c086e4c45995db84918fe612c863c
Ankit Jaiswal <ankitjaiswal12@gmail.com>
2021-03-30 11:32:11 +0530

    Update FindCommand.php

--- src/Commands/FindCommand.php
+++ src/Commands/FindCommand.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Themsaid\Langman\Commands;
+namespace Ankitjaiswal\Langman\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
-use Themsaid\Langman\Manager;
+use Ankitjaiswal\Langman\Manager;
 use Illuminate\Support\Str;
 
 class FindCommand extends Command
@@ -26,7 +26,7 @@ class FindCommand extends Command
     /**
      * The Languages manager instance.
      *
-     * @var \Themsaid\LangMan\Manager
+     * @var \Ankitjaiswal\LangMan\Manager
      */
     private $manager;
 
@@ -40,7 +40,7 @@ class FindCommand extends Command
     /**
      * ListCommand constructor.
      *
-     * @param \Themsaid\LangMan\Manager $manager
+     * @param \Ankitjaiswal\LangMan\Manager $manager
      * @return void
      */
     public function __construct(Manager $manager)
